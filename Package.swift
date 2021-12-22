7136c0c085020c9406a5757fc07c572f5472efdd
Sergey Khliustin <sergey.khliustin@gmail.com>
2021-12-22 21:18:59 +0200

    Playing with packages

--- Package.swift
+++ Package.swift
@@ -14,14 +14,30 @@ let package = Package(
             targets: ["BuildioMain"])
     ],
     dependencies: [
-        .package(name: "Models", path: "Modules/Models"),
-        .package(name: "BitriseAPIs", path: "Modules/BitriseAPIs"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.0.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
         .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", branch: "master"),
-        .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "0.1.0")
+        .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "0.1.0"),
+        .package(name: "SwiftyBeaver", url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.9.5")
     ],
     targets: [
+        .target(
+            name: "Logger",
+            dependencies: ["SwiftyBeaver"],
+            path: "Modules/Logger/Sources/Logger"
+        ),
+        .target(
+            name: "Models",
+            path: "Modules/Models/Sources/Models"
+        ),
+        .target(
+            name: "BitriseAPIs",
+            dependencies: ["Models", "Logger"],
+            path: "Modules/BitriseAPIs/Sources/BitriseAPIs",
+            resources: [
+                .copy("DemoData")
+            ]
+        ),
         .target(
             name: "BuildioMain",
             dependencies: [
