41817a1fe2ba096adc4c9e783ad5430998621d22
Sergey Khliustin <sergey.khliustin@gmail.com>
2021-12-22 20:40:22 +0200

    Playing with packages

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildioApp",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "BuildioApp",
+            targets: ["BuildioApp"])
+    ],
+    dependencies: [
+        .package(name: "BuildioMain", path: "Modules/BuildioMain")
+    ],
+    targets: [
+        .target(
+            name: "BuildioApp",
+            dependencies: [
+                "BuildioMain"
+            ], path: "Shared",
+            sources: [])
+    ]
+)
