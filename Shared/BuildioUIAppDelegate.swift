635285e22f778f1627c9227a84b6067958c6aeba
Sergey Khliustin <sergey.khliustin@gmail.com>
2022-03-21 23:44:15 +0800

    code cleanup; assets optimisations

--- Shared/BuildioUIAppDelegate.swift
+++ Shared/BuildioUIAppDelegate.swift
@@ -12,7 +12,6 @@ import UserNotifications
 import BuildioLogic
 
 public final class BuildioUIAppDelegate: NSObject, UIApplicationDelegate {
-    var statusBarPlugin: MacStatusBarPluginProtocol?
     
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         #if targetEnvironment(macCatalyst)
@@ -26,27 +25,5 @@ public final class BuildioUIAppDelegate: NSObject, UIApplicationDelegate {
         center.removeAllPendingNotificationRequests()
         
         return true
-    }
-    
-    private func loadPlugin() {
-        let bundleFileName = "MacStatusBarPlugin.bundle"
-        guard let bundleURL = Bundle.main.builtInPlugInsURL?
-                .appendingPathComponent(bundleFileName) else { return }
-        
-        guard let bundle = Bundle(url: bundleURL) else { return }
-        
-        let className = "MacStatusBarPlugin.MacStatusBarPlugin"
-        guard let pluginClass = bundle.classNamed(className) as? MacStatusBarPluginProtocol.Type else { return }
-
-        let plugin = pluginClass.init { action, _, _  in
-            switch action {
-            case .newWindow:
-                UIApplication.shared.requestSceneSessionActivation(nil, userActivity: nil, options: nil) { error in
-                    
-                }
-            }
-        }
-        plugin.setup()
-        self.statusBarPlugin = plugin
     }
 }
